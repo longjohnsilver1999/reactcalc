1c11ecc1a5725a64ef0253cd1e37d35cd156c648
sumitk-hub1999 <skatyal1999@gmail.com>
2023-02-13 15:21:30 +0530

    styling done

--- src/App.js
+++ src/App.js
@@ -1,27 +1,32 @@
+import "./style.css";
 function App() {
   return (
     <div className="calculator-container">
       <div className="output-display">
-        <div className="operand1"></div>
-        <div className="operand2"></div>
+        <div className="operand1">12345</div>
+        <div className="operand2">12345</div>
       </div>
+
       <button className="C">C</button>
       <button className="del">DEL</button>
       <button>1</button>
       <button>2</button>
+
+      <button className="op-btn">/</button>
       <button>3</button>
-      <button>/</button>
       <button>4</button>
+      <button className="op-btn">*</button>
       <button>5</button>
       <button>6</button>
-      <button>*</button>
+      <button className="op-btn">+</button>
       <button>7</button>
       <button>8</button>
+      <button className="op-btn">-</button>
       <button>9</button>
-      <button>+</button>
+
       <button>0</button>
       <button>.</button>
-      <button>-</button>
+
       <button className="op">=</button>
     </div>
   );
